4a2b1cc0ea740216fd80eef4dd32f51fb8b10e56
Ani Mihaylova <anivm.webdev@gmail.com>
2022-09-16 14:57:40 -0400

    7steps addition for employee engagements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Comments from './components/Comments';
+// import FileUpload from './components/FileUpload';
+import Goals from './components/Goals';
+import Header from './components/Header';
+import Values from './components/Values';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="content">
+        <Values />
+        <Goals />
+        <Comments />
+      </div>
+    </>
   );
 }
 
